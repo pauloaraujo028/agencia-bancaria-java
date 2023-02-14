1f0a0e8cab9d3bb9abe499a0e26768172ad69867
Paulo Araújo <pauloaraujo.phz@gmail.com>
2023-02-14 15:29:10 -0300

    first commit

---
+++ src/project/Conta.java
@@ -0,0 +1,80 @@
+package project;
+
+import utilitarios.Utils;
+
+public class Conta {
+
+    private static int contadorDeContas = 1;
+
+    private int numeroConta;
+    private Cliente cliente;
+    private Double saldo = 0.0;
+
+    public Conta(Cliente cliente) {
+        this.numeroConta = contadorDeContas;
+        this.cliente = cliente;
+        contadorDeContas += 1;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String toString() {
+        return "\nNúmero da conta: " + this.getNumeroConta() +
+                "\nNome: " + this.cliente.getNome() +
+                "\nCPF: " + this.cliente.getCPF() +
+                "\nEmail: " + this.cliente.getEmail() +
+                "\nSaldo: " + Utils.doubleToString(this.getSaldo()) +
+                "\n";
+    }
+
+    public void depositar(Double valor) {
+        if(valor > 0) {
+            setSaldo(getSaldo() + valor);
+            System.out.println("Seu depósito foi realizado com sucesso!");
+        }else {
+            System.out.println("Não foi possível realizar o depósito!");
+        }
+    }
+
+    public void sacar(Double valor) {
+        if(valor > 0 && this.getSaldo() >= valor) {
+            setSaldo(getSaldo() - valor);
+            System.out.println("Saque realizado com sucesso!");
+        }else {
+            System.out.println("Não foi possível realizar o saque!");
+        }
+    }
+
+    public void transferir(Conta contaParaDeposito, Double valor) {
+        if(valor > 0 && this.getSaldo() >= valor) {
+            setSaldo(getSaldo() - valor);
+
+            contaParaDeposito.saldo = contaParaDeposito.getSaldo() + valor;
+            System.out.println("Transferencia realizada com sucesso!");
+        }else {
+            System.out.println("Não foi possível realizar a transferencia!");
+        }
+    }
+}
